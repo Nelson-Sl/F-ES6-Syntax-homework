76b0d7653de028163e58f7f18ce34ed111a2ad2b
Shen Liao <shen.liao@thoughtworks.com>
2020-09-11 20:52:10 +0800

    [Nelson Liao]: finish javascript exercise

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = (input) => {
+    const output = [];
+    for(let data of input.data) {
+        let object = {};
+        data.map((attributeData,index) => {
+            object[input.column[index].name] = attributeData;
+        })
+        output.push(object);
+    }
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (items, sections) => {
+    let currentMaxInsertPos = 0;
+    let count = 0;
+    for(let section of sections) {
+        if(section.index > currentMaxInsertPos) {
+            count += 1;
+            currentMaxInsertPos = section.index;
+        }
+        items.splice(section.index+count,0,section.content);
+    }
+    return items;
+}
 export { inject };
