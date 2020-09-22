25934200eef7c180aa5cf973a4fd07717e2f7715
kevindongzg <dongkevin94@gmail.com>
2020-09-22 10:01:40 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,10 @@
+// TODO 使用解构的方式取出data与column
 const parseData = (input) => {
+    // TODO 使用map做数组与数组的转化，不用声明output
     const output = [];
     for(let data of input.data) {
         let object = {};
+        // TODO 不是map使用场景，查查map的用法
         data.map((attributeData,index) => {
             object[input.column[index].name] = attributeData;
         })
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO 思考先对sections 进行排序，再使用带有index的遍历可以优化
     let currentMaxInsertPos = 0;
     let count = 0;
     for(let section of sections) {
